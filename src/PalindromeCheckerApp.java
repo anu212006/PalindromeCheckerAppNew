d36c83eefa41dea4d0ffaa86bc0e1574c1731fd7
anu212006 <anushkaverma212006@gmail.com>
2026-03-17 14:54:14 +0530

    UC8: Linked list palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,65 @@
-import java.util.ArrayDeque;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
 
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
     public static void main(String[] args) {
 
-        String str = "racecar";
-        ArrayDeque<Character> deque = new ArrayDeque<>();
+        String str = "madam";
 
-        for(char c : str.toCharArray()) {
-            deque.add(c);
+        Node head = new Node(str.charAt(0));
+        Node temp = head;
+
+        for (int i = 1; i < str.length(); i++) {
+            temp.next = new Node(str.charAt(i));
+            temp = temp.next;
         }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
 
         boolean isPalindrome = true;
 
-        while(deque.size() > 1) {
-            if(!deque.removeFirst().equals(deque.removeLast())) {
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 isPalindrome = false;
                 break;
             }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
 
-        if(isPalindrome) {
+        if (isPalindrome)
             System.out.println("Palindrome");
-        } else {
+        else
             System.out.println("Not Palindrome");
-        }
     }
 }
