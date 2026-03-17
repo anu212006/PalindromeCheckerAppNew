e295d19d817d309f3705ecb86f637aeba5249452
anu212006 <anushkaverma212006@gmail.com>
2026-03-17 14:55:40 +0530

    UC11: OOP palindrome service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,25 @@
-public class PalindromeCheckerApp {
+class PalindromeService {
 
-    public static void main(String[] args) {
-
-        String str = "Madam In Eden Im Adam";
-
-        str = str.replaceAll("\\s+", "").toLowerCase();
-
+    boolean checkPalindrome(String str) {
         String rev = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
             rev += str.charAt(i);
         }
 
-        if (str.equals(rev))
+        return str.equals(rev);
+    }
+}
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        PalindromeService service = new PalindromeService();
+
+        String str = "madam";
+
+        if (service.checkPalindrome(str))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
