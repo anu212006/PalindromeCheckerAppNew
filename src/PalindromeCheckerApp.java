c537f1c49bf0f00edf85d7666ddd242013f7816a
anu212006 <anushkaverma212006@gmail.com>
2026-03-17 14:40:12 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,8 +2,21 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC1 - Welcome Message
+        // UC1
         System.out.println("Welcome to Palindrome Checker App");
 
+        // UC2 - Hardcoded Palindrome
+        String str = "madam";
+        String rev = "";
+
+        for(int i = str.length()-1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if(str.equals(rev)) {
+            System.out.println(str + " is Palindrome");
+        } else {
+            System.out.println(str + " is Not Palindrome");
+        }
     }
 }
