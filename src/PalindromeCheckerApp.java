80588395244c12268e8c5d7a6df83fb38e48a6c5
anu212006 <anushkaverma212006@gmail.com>
2026-03-17 14:55:18 +0530

    UC10: Ignore case and spaces

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,18 @@
 public class PalindromeCheckerApp {
 
-    static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return isPalindrome(str, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
 
-        String str = "level";
+        String str = "Madam In Eden Im Adam";
 
-        if (isPalindrome(str, 0, str.length() - 1))
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        if (str.equals(rev))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
