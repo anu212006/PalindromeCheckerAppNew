4f86afbf52f4f35625240afe3b0bf769821b85c0
anu212006 <anushkaverma212006@gmail.com>
2026-03-17 14:56:28 +0530

    UC13: Performance comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,51 +1,21 @@
-interface PalindromeStrategy {
-    boolean check(String str);
-}
+public class PalindromeCheckerApp {
 
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String str) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-
-        for (char c : str.toCharArray())
-            stack.push(c);
-
+    static boolean reverseMethod(String str) {
         String rev = "";
-
-        while (!stack.isEmpty())
-            rev += stack.pop();
-
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
         return str.equals(rev);
     }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean check(String str) {
-        java.util.ArrayDeque<Character> deque = new java.util.ArrayDeque<>();
-
-        for (char c : str.toCharArray())
-            deque.add(c);
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast()))
-                return false;
-        }
-        return true;
-    }
-}
-
-public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        PalindromeStrategy strategy = new StackStrategy();
-
         String str = "madam";
 
-        if (strategy.check(str))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        long start = System.nanoTime();
+        reverseMethod(str);
+        long end = System.nanoTime();
+
+        System.out.println("Execution Time: " + (end - start) + " ns");
     }
 }
